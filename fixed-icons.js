16f19107ed35552ca61af20f3fc916fb6173e09a
Vibha Seekhwal <your_github_email@gmail.com>
2026-02-04 15:12:39 +0530

    Updated files

--- fixed-icons.js
+++ fixed-icons.js
@@ -1,11 +1,20 @@
-  const socialContainer = document.getElementById('socialContainer');
-  const mainBtn = document.getElementById('mainBtn');
-  const closeBtn = document.getElementById('closeBtn');
+document.addEventListener("DOMContentLoaded", () => {
+  const socialContainer = document.getElementById("socialContainer");
+  const mainBtn = document.getElementById("mainBtn");
+  const closeBtn = document.getElementById("closeBtn");
 
-  mainBtn.addEventListener('click', () => {
-    socialContainer.classList.toggle('active');
+  if (!socialContainer || !mainBtn || !closeBtn) {
+    console.error("Social buttons elements not found!");
+    return;
+  }
+
+  // Open / Toggle social icons
+  mainBtn.addEventListener("click", () => {
+    socialContainer.classList.toggle("active");
   });
 
-  closeBtn.addEventListener('click', () => {
-    socialContainer.classList.remove('active');
+  // Close social icons
+  closeBtn.addEventListener("click", () => {
+    socialContainer.classList.remove("active");
   });
+});
